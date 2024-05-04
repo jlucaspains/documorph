145b564c358ffdf2c9661784dcb8ee4703bc3f3f
jlucaspains <jlucaspains@gmail.com>
2024-05-04 14:39:18 -0600

    More tests

--- test/documorph.tests/ProgramTests.cs
+++ test/documorph.tests/ProgramTests.cs
@@ -39,6 +39,16 @@ public class ProgramTests
     }
 
     [Fact]
+    public void WillConvertSimpleDocx()
+    {
+        var entryPoint = typeof(Program).Assembly.EntryPoint!;
+        var result = entryPoint.Invoke(null, [new string[] { "md", "--in", "test_data/simpletest.docx", "--out", "test_data/WillConvertSimpleDocx.md" }]);
+
+        Assert.Equal(0, result);
+        Assert.True(File.Exists("test_data/WillConvertSimpleDocx.md"));
+    }
+    
+    [Fact]
     public void ValidParameters()
     {
         var entryPoint = typeof(Program).Assembly.EntryPoint!;
@@ -47,6 +57,17 @@ public class ProgramTests
         Assert.Equal(0, result);
         Assert.True(File.Exists("test_data/Test_ValidParameters.md"));
         Assert.True(File.Exists("test_data/image1.png"));
+    }
+    
+    [Fact]
+    public void WillCreateOutputDirectoryIfNeeded()
+    {
+        var entryPoint = typeof(Program).Assembly.EntryPoint!;
+        var result = entryPoint.Invoke(null, [new string[] { "md", "--in", "test_data/docconverter.docx", "--out", "test_data2/Test_ValidParameters.md" }]);
+
+        Assert.Equal(0, result);
+        Assert.True(File.Exists("test_data2/Test_ValidParameters.md"));
+        Assert.True(File.Exists("test_data2/image1.png"));
     }
     
     [Fact]
